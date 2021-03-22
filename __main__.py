2dd585dc41f397b5883557a572abaf6f65b09cb9
nuvulanaresh <31900328+nuvulanaresh@users.noreply.github.com>
2021-03-22 00:06:43 -0500

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ def elevator_commute(elevator):
     print("Elevator Starting Floor:", elevator.start_floor)
     print("Elevator is going",elevator.direction)
     print("Please enter the desired floors 1-",elevator.total_floors)
-    print("Enter Floor# as -1 when you are done")
+    print("Enter Floor# as -1 to close the Elevator door")
     elevator.floors_requested()
 
 #Main function
@@ -20,14 +20,14 @@ def main():
     # elevator=Elevator(total_floors=5,start_floor=2,direction='up')
     # elevator_commute(elevator)
     #
-    elevator=Elevator(total_floors=5,start_floor=4,direction='down')
-    elevator_commute(elevator)
+    # elevator=Elevator(total_floors=5,start_floor=4,direction='down')
+    # elevator_commute(elevator)
     #
     # elevator=Elevator(total_floors=5,start_floor=3,direction='up')
     # elevator_commute(elevator)
 
-    # elevator=Elevator(total_floors=5,start_floor=3,direction='down')
-    # elevator_commute(elevator)
+    elevator=Elevator(total_floors=10,start_floor=1,direction='up')
+    elevator_commute(elevator)
 
 
 if __name__ == '__main__':
