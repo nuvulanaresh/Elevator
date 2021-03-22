1ee348cecc5185789d932718f9193b2fdb237f2b
nuvulanaresh <31900328+nuvulanaresh@users.noreply.github.com>
2021-03-22 16:19:23 -0500

    Add files via upload
    Added validations

--- __main__.py
+++ __main__.py
@@ -4,16 +4,18 @@ from elevator import Elevator
 def elevator_commute(elevator):
     print("Welcome to the ANZ Elevator!!")
     print("Number of floors for the elevator:", elevator.total_floors)
-    print("Elevator Starting Floor:", elevator.start_floor)
+    print("Elevator is at floor #"+str(elevator.start_floor))
     print("Elevator is going",elevator.direction)
     print("Please enter the desired floors 1-",elevator.total_floors)
-    print("Enter Floor# as -1 to close the Elevator door")
     elevator.floors_requested()
 
 #Main function
 def main():
-    #elevator object instantiated from Elevator class
+    # elevator object instantiated from Elevator class
     # elevator=Elevator(total_floors=5,start_floor=1,direction='up')
+    # elevator_commute(elevator)
+
+    # elevator=Elevator(total_floors=5,start_floor=5,direction='down')
     # elevator_commute(elevator)
 
     # Unit Tests
@@ -26,9 +28,11 @@ def main():
     # elevator=Elevator(total_floors=5,start_floor=3,direction='up')
     # elevator_commute(elevator)
 
-    elevator=Elevator(total_floors=10,start_floor=1,direction='up')
-    elevator_commute(elevator)
+    # elevator=Elevator(total_floors=10,start_floor=3,direction='up')
+    # elevator_commute(elevator)
 
+    # elevator=Elevator(total_floors=10,start_floor=7,direction='down')
+    # elevator_commute(elevator)
 
 if __name__ == '__main__':
     main()
--- elevator.py
+++ elevator.py
@@ -1,35 +1,39 @@
 class Elevator:
-    # Initializing the elevator variables
+    # Initializing the elevator behaviour variables
     def __init__(self,total_floors,start_floor,direction):
         #Total Floors for the Elevator
         self.total_floors=total_floors
         #Floor Starting position
         self.start_floor=start_floor
-        #Initalizing the floor list
+        #Initalizing the requested floor list
         self.floor_list=[]
         #Door close option indicating the end of floor inputs from passengers
         self.door_close=-1
         #Tracks current floor while elevator is in motion
         self.track_floor=start_floor
-        #Initializing the floors
+        #Initializing the floors variable
         self.floors=0
         #Direction whether 'up' or 'down'
         self.direction=direction
 
-    #function which takes floor inputs from people
+    #function which takes floor inputs from people and does input validations
     def floors_requested(self):
         while (self.floors != self.door_close):
             while True:
                 try:
                     #floor inputs validation
-                    self.floors = int(input("Enter Floor#:"))
-                except ValueError:
-                    print("Please enter valid input")
+                    self.floors = int(input("Enter Floor# (or) -1 for door close:"))
+                except:
+                    print("Please enter -1 for door close (or) desired floors 1-",self.total_floors)
+                    continue
+                if (self.floors not in range(1, self.total_floors + 1) and self.floors!=-1):
+                    print("Please enter -1 for door close (or) desired floors 1-",self.total_floors)
                     continue
                 break
+
             if (self.floors == self.door_close):
                 break
-            elif((self.direction=='up' and self.floors>self.start_floor) or (self.direction == 'down' and self.floors<self.start_floor)):
+            elif((self.direction=='up' and self.floors>self.start_floor and self.floors <= self.total_floors) or (self.direction == 'down' and self.floors<self.start_floor and self.floors > 0)):
                 self.floor_list.append(self.floors)
         #depending on the direction, the elevator will travel either up or down
         if(self.direction=='up'):
